f20e0d0304bb4f4c4cb8b31a7c604d37d83e03c8
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-06-02 12:12:41 -0600

    feat: Custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="min-h-screen bg-black">
+      <h1 className="text-center text-6xl text-white pt-[20rem]">Not found</h1>
+      <h2 className="bg-gradient-to-r from-sky-500 via-purple-400 to-purple-600 text-transparent bg-clip-text text-center text-5xl">404</h2>
+    </div>
+  );
+};
+
+export default NotFound;
