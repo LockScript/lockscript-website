b7b4a5011ff363d96dc0a33aa01707e4b3917cf5
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-06-20 12:12:49 -0600

    chore: Update deps and also cleaned up some imports.

--- src/app/(api)/api/contact/route.ts
+++ src/app/(api)/api/contact/route.ts
@@ -1,7 +1,5 @@
-import { error } from 'console';
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server';
-import { Resend } from "resend"
+import { Resend } from "resend";
 
 const resend: any = new Resend(process.env.RESEND_API_KEY)
 
